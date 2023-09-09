91ed651eb6177ebe61c8c6eb597c87a816b737a6
mamichels <info@marcelmichels.de>
2023-09-09 22:25:07 +0200

    Add tests

--- test/commands/calc.test.ts
+++ test/commands/calc.test.ts
@@ -1,17 +1,32 @@
 import {expect, test} from '@oclif/test'
 
 describe('calc', () => {
-  test
-  .stdout()
-  .command(['calc'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
 
   test
-  .stdout()
-  .command(['calc', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['calc', '1 + 1'])
+    .it('runs addition', ctx => {
+      expect(ctx.stdout).to.contain('2')
+    });
+
+  test
+    .stdout()
+    .command(['calc', '1 + 1'])
+    .it('runs addition', ctx => {
+      expect(ctx.stdout).to.contain('2')
+    });
+
+  test
+    .stdout()
+    .command(['calc', '2 - 1'])
+    .it('runs subtraction', ctx => {
+      expect(ctx.stdout).to.contain('1')
+    });
+
+  test
+    .stdout()
+    .command(['calc', '2 / 3'])
+    .catch(error => expect(error.message).to.contain('No supported operation found in calculation string: 2 / 3'))
+    .it('runs no supported operation');
+
 })
